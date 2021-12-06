9fdb7482a2cd969df27b6e73b82d513436ac0cff
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-06 14:42:40 -0700

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -2,6 +2,9 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$cfg['suppress_issue_types'] = [];
+$cfg['suppress_issue_types'] = [
+	'SecurityCheck-LikelyFalsePositive',
+	'PhanPluginMixedKeyNoKey'
+];
 
 return $cfg;
