b650eeb36c2a5162a62236e9d6ba61c28c0beab1
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-19 13:16:12 -0700

    Ignore PhanAccessMethodInternal

--- .phan/config.php
+++ .phan/config.php
@@ -3,8 +3,9 @@
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['suppress_issue_types'] = [
+	'PhanAccessMethodInternal',
+	'PhanPluginMixedKeyNoKey',
 	'SecurityCheck-LikelyFalsePositive',
-	'PhanPluginMixedKeyNoKey'
 ];
 
 return $cfg;
