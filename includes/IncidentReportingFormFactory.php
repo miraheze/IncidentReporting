29c6d9dccd9f1741fb8c8639092d16ff506aa8d5
MacFan4000 <paulfkeffer@gmail.com>
2025-09-10 00:31:27 -0400

    Update IncidentReportingFormFactory.php

--- includes/IncidentReportingFormFactory.php
+++ includes/IncidentReportingFormFactory.php
@@ -84,12 +84,14 @@ class IncidentReportingFormFactory {
 
 		$responders = [];
 
+		$userLinkRenderer = MediaWikiServices::getInstance()->getUserLinkRenderer();
+
 		if ( $data !== null ) {
 			$respArray = explode( "\n", $data->i_responders );
 
 			if ( count( $respArray ) != 0 ) {
 				foreach ( $respArray as $resp ) {
-					$responders[] = UserLinkRenderer::userLink( new UserIdentityValue( $userFactory->newFromName( $resp )->getId(), $resp ), $context );
+					$responders[] = $userLinkRenderer::userLink( new UserIdentityValue( $userFactory->newFromName( $resp )->getId(), $resp ), $context );
 				}
 			}
 		}
@@ -112,9 +114,9 @@ class IncidentReportingFormFactory {
 
 			foreach ( $dbReviewers as $db ) {
 				if ( $db->r_timestamp ) {
-					$reviewers['reviewed'][] = UserLinkRenderer::userLink( new UserIdentityValue( $userFactory->newFromName( $db->r_user )->getId(), $db->r_user ), $context );
+					$reviewers['reviewed'][] = $userLinkRenderer::userLink( new UserIdentityValue( $userFactory->newFromName( $db->r_user )->getId(), $db->r_user ), $context );
 				} else {
-					$reviewers['unreviewed'][] = UserLinkRenderer::userLink( new UserIdentityValue( $userFactory->newFromName( $db->r_user )->getId(), $db->r_user ), $context );
+					$reviewers['unreviewed'][] = $userLinkRenderer::userLink( new UserIdentityValue( $userFactory->newFromName( $db->r_user )->getId(), $db->r_user ), $context );
 				}
 
 				$reviewers['all'][] = $db->r_user;
